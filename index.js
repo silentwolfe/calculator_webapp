98db81871b8c056c0f937a85a31efd8d4bb22ef0
Eboizegbe Osaro <oeboizegbe@gmail.com>
2024-11-20 16:44:00 +0100

    Incomplete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const 
+const display = document.getElementById("display");
+const buttons = [one, two, three, four, five, six, seven, eight, nine, zero, dot, ast];
+
+buttons.forEach(button, () => {
+    button.addEventListener('click', () => {
+        display.innerText += buttons.innerText;
+    });
+});
