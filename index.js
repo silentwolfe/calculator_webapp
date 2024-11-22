9e62da21ce8b0db5f997081faaf33ce79a7a8b33
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2024-11-22 16:26:15 +0100

    In-complete

--- index.js
+++ index.js
@@ -1,8 +1,85 @@
 const display = document.getElementById("display");
-const buttons = [one, two, three, four, five, six, seven, eight, nine, zero, dot, ast];
+const operators = [add, mul, div, sub, equal, perc, del, clear];
+const hide = document.getElementById("hide");
+// const buttons = [one, two, three, four, five, six, seven, eight, nine, zero, dot, ast];
+const buttons = document.querySelectorAll(".grid-item");
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
 
-buttons.forEach(button, () => {
+buttons.forEach(button => {
     button.addEventListener('click', () => {
-        display.innerText += buttons.innerText;
+        display.innerText += button.innerText;
     });
+});
+
+clear.addEventListener("click", () => {
+    display.innerText = '';
+});
+
+add.addEventListener("click", () => {
+    operator = '+';
+    firstNumber = display.innerText;
+    display.innerText = "";
+});
+
+sub.addEventListener("click", () => {
+    operator = '-';
+    firstNumber = display.innerText;
+    display.innerText = "";
+});
+
+mul.addEventListener("click", () => {
+    operator = '*';
+    firstNumber = display.innerText;
+    display.innerText = "";
+});
+
+div.addEventListener("click", () => {
+    operator = '/';
+    firstNumber = display.innerText;
+    display.innerText = "";
+});
+
+perc.addEventListener("click", () => {
+    if (display.innerText != "") {
+        if (operator && firstNumber) {
+            secondNumber = display.innerText;
+            display.innerText = (parseFloat(firstNumber) * parseFloat(secondNumber)) / 100;
+        } 
+        else {
+            display.innerText = parseFloat(display.innerText) / 100;
+        }
+    }
+});
+
+del.addEventListener("click", () => {
+    let updatedText = display.innerText.slice(0, -1);
+    display.innerText = updatedText;
+})
+equal.addEventListener("click", () => {
+    secondNumber = display.innerText;
+    let result;
+
+    switch(operator) {
+        case '+':
+            result = parseInt(firstNumber) + parseInt(secondNumber);
+            break;
+        case '-':
+            result = parseFloat(firstNumber) - parseFloat(secondNumber);
+            break;
+        case '*':
+            result = parseFloat(firstNumber) * parseFloat(secondNumber);
+            break;
+        case '/':
+            result = parseFloat(firstNumber) / parseFloat(secondNumber);
+            break;
+        case '%':
+            result = perc;
+        default:
+            result = "Invalid Operation";
+    }
+    display.innerText = result;
+    firstNumber = "0";
+    secondNumber = "0";
 });
